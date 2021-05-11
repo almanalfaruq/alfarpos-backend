305daaef2afe335c8ced02b3856f6963cf6c331c
Almantera Tiantana Al Faruqi <alman.alfaruq@gmail.com>
2021-05-12 04:28:14 +0700

    Updating install script

--- install.sh
+++ install.sh
@@ -4,15 +4,21 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 BINARY="/home/$SUDO_USER/go/bin/alfarpos-backend"
+INSTALLMESSAGE="Installing alfarpos-backend"
+UPDATE=0
 if [ -f $BINARY ]; then
-  echo "Removing existing binary and updating with the new one"
-  rm $BINARY
+  INSTALLMESSAGE="Updating alfarpos-backend"
+  UPDATE=1
 fi
 echo "Installing alfarpos-backend"
 sudo -u $SUDO_USER /usr/local/go/bin/go install -v
 if [ ! -f $BINARY ]; then
   echo "There's an error when installing the binary"
   exit 1
+fi
+if [ $UPDATE -e 1]; then
+  echo "Success updating alfarpos-backend binary"
+  exit 0
 fi
 echo "Setting up alfarpos log dir"
 LOGDIR="/var/log/alfarpos/"
