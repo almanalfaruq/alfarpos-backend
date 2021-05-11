77112061c511953f6825762657cb3936da516aee
Almantera Tiantana Al Faruqi <alman.alfaruq@gmail.com>
2021-05-12 04:46:31 +0700

    Fix and update install script

--- install.sh
+++ install.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
+
+## Check root
 if [[ $EUID -ne 0 ]]; then
   echo "Must be run as a root user"
   exit 1
 fi
-BINARY="/home/$SUDO_USER/go/bin/alfarpos-backend"
+
+## Step 1 - Instal binary to $GOPATH folder
+BINARY="$GOPATH/bin/alfarpos-backend"
 INSTALLMESSAGE="Installing alfarpos-backend"
-UPDATE=0
+UPDATE=0 ## Update if any
 if [ -f $BINARY ]; then
   INSTALLMESSAGE="Updating alfarpos-backend"
   UPDATE=1
 fi
-echo "Installing alfarpos-backend"
+echo $INSTALLMESSAGE
 sudo -u $SUDO_USER /usr/local/go/bin/go install -v
 if [ ! -f $BINARY ]; then
   echo "There's an error when installing the binary"
   exit 1
 fi
-if [ $UPDATE -e 1]; then
+if [ $UPDATE -eq 1 ]; then
   echo "Success updating alfarpos-backend binary"
   exit 0
 fi
+
+## Steps below for installing a new binary
+## Step 2 - Setting log directory
 echo "Setting up alfarpos log dir"
 LOGDIR="/var/log/alfarpos/"
 if [ ! -d "$LOGDIR" ]; then
@@ -27,6 +34,8 @@ if [ ! -d "$LOGDIR" ]; then
   mkdir -p $LOGDIR
   chown $SUDO_USER:$SUDO_USER $LOGDIR
 fi
+
+## Step 3 - Setting config file
 DIRCONFIG="/etc/alfarpos/"
 CONFIGFILE="config.yaml"
 echo "Copying config file. Don't forget to change the config in $DIRCONFIG$CONFIGFILE"
@@ -40,5 +49,7 @@ if [ -f $DIRCONFIG$CONFIGFILE ]; then
   echo "Backup created at $DIRCONFIG$CONFIGFILE.bk"
 fi
 cp "./files/etc/alfarpos/config.yaml" $DIRCONFIG
+
+## Step 4 - Setting service file
 echo "Setting up service file. You should start and enable it by yourself"
 cp "./files/etc/systemd/system/alfarpos.service" "/etc/systemd/system/"
