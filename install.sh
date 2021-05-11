afd4dd1e9b592362da7d7656e469dff2b6b901b6
Almantera Tiantana Al Faruqi <alman.alfaruq@gmail.com>
2021-05-11 16:36:39 +0700

    Updating install.sh script

--- install.sh
+++ install.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+  echo "Must be run as a root user"
+  exit 1
+fi
+BINARY="/home/$SUDO_USER/go/bin/alfarpos-backend"
+if [ -f $BINARY ]; then
+  echo "Removing existing binary and updating with the new one"
+  rm $BINARY
+fi
 echo "Installing alfarpos-backend"
-go install -v
+sudo -u $SUDO_USER /usr/local/go/bin/go install -v
+if [ ! -f $BINARY ]; then
+  echo "There's an error when installing the binary"
+  exit 1
+fi
 echo "Setting up alfarpos log dir"
 LOGDIR="/var/log/alfarpos/"
 if [ -d "$LOGDIR" ]; then
+  echo "Log directory didn't exist, creating the directory"
   mkdir -p $LOGDIR
-  chown root:$USER $LOGDIR
+  chown root:$SUDO_USER $LOGDIR
 fi
 echo "Copying config file"
 mkdir -p "/etc/alfarpos/"
